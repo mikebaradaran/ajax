ffef32ce8aef321425a2906dc4f44cb734ed382a
mikebaradaran <mike.baradaran@qa.com>
2024-02-22 14:04:23 +0000

    first commit

---
+++ students.js
@@ -0,0 +1,15 @@
+﻿let names = `Wraith, Lee
+Adu - Baah, Joseph
+Khan, Salman
+Turner, Kush
+Swarbrick, Joseph
+Yousaf, Ibraheem
+Whitehead, Christopher
+Kang, Hae Joo
+Bulmer, Jack
+Snape, Jack
+Sweetman, Ben
+Tobin, Alex
+Flitcroft, Ivan
+Lewis, Joseph`;
+names = names.split("\n");
